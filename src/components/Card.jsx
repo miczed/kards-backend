52c283378d5a8b28120ae89a1184935b53fb18b8
Michael Ziörjen <mighty.miczed@gmail.com>
2017-03-10 12:13:16 +0100

    Implemented basic turn card functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,19 @@
 import React, {Component} from 'react';
 export default class Card extends Component {
+    constructor() {
+        super();
+        this.state = {
+            front: true,
+        }
+    }
+    clickHandler = () => {
+      this.setState({front: !this.state.front});
+    };
     render() {
         return(
-            <div className="card"><p>{this.props.text}</p></div>
+            <div className="card" onClick={this.clickHandler}>
+                { this.state.front ? <div className="front">{this.props.text}</div> : <div className="front">Backside.</div>}
+            </div>
         );
     }
 }
